1fe421c3a4b60f9e93c89fdddcdf2756586d8da3
daniesky <daniesky@stud.ntnu.no>
2023-03-06 13:30:03 +0100

    Websocket server now multithreaded and can send messages to all and individual clients.

--- practice_six/websocket_java/src/main/java/WebSocket.java
+++ practice_six/websocket_java/src/main/java/WebSocket.java
@@ -8,6 +8,8 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,97 +20,100 @@ import java.util.regex.Pattern;
  */
 public class WebSocket {
 
-    public void start() throws IOException {
+    private HashMap <String, Socket> clientMap = new HashMap<>();
+
+    public void start() throws IOException, NoSuchAlgorithmException {
         ServerSocket server = new ServerSocket(3001);
         System.out.println("Established a server socket on 127.0.0.1:3001. \n Waiting for connection");
-        try {
-            Socket client = server.accept();
-            System.out.println("A client has connected to the socket");
+        Thread sendThread = new Thread(new Runnable(){
 
-            //Retrieve streams from the new client socket. This is the streams where we can retrieve/send data.
-
-            InputStream in = client.getInputStream();
-            OutputStream out = client.getOutputStream();
-            Scanner sc = new Scanner(in, "UTF-8");
-
-            //Retrieve the initial message from client and verifying that it matches the pattern of a GET request.
-            String data = sc.useDelimiter("\\r\\n\\r\\n").next();
-            System.out.println("Following request received:");
-            System.out.println(data);
-            Matcher get = Pattern.compile("^GET").matcher(data);
-
-            //Code only runs if the pattern matches get request.
-            if(get.find()) {
-
-                //Retrieve the websocket key header data.
-                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-                match.find();
-
-                //We create the HTTP response that we will send to the client. We format it according to RFC6455.
-                byte[] response = (
-                        "HTTP/1.1 101 Switching Protocols\r\n"
-                                + "Connection: Upgrade\r\n"
-                                + "Upgrade: websocket\r\n"
-                                + "Sec-WebSocket-Accept: "
-                                + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1")
-                                .digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
-                                + "\r\n\r\n").getBytes("UTF-8");
-
-                //Write the response to the output stream.
-                System.out.println("Responding with:");
-                System.out.println(new String(response, StandardCharsets.UTF_8));
-                out.write(response, 0, response.length);
-                out.flush();
-
-                Thread readThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        while(true){
-                            try {
-                                String received = decryptInput(in);
-                                if(!received.isBlank() && !received.isEmpty()){
-                                    System.out.println("Message received from client: " + received);
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+            @Override
+            public void run() {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Server side message application\nIf you want to relay messages to client, type message and hit enter. ");
+                while(true){
+                    String in = sc.nextLine();
+                    if(!in.isBlank() && !in.isBlank()){
+                        try{
+                            globalMessage(in);
+                        }
+                        catch(IOException e){
+                            System.out.println("Error occured when transmitting message");
+                            e.printStackTrace();
                         }
                     }
                 }
-                );
-                readThread.start();
-                while (true) {
-                    Scanner readServer = new Scanner(System.in);
-                    String mes = readServer.nextLine();
-                    if(!mes.isEmpty() && !mes.isBlank()){
-                        this.sendWebSocketMessage(mes, out);
-                    }
-
-                }
-
             }
+        } );
+        sendThread.start();
+        //Infinite loop for catching new connections.
+        while (true) {
+            Socket client = null;
+            try {
+                client = server.accept();
+            } catch (IOException e) {
+                System.out.println("Socket establish failed.");
+            }
+            if (client != null) {
+                System.out.println("A client has connected to the socket");
 
+                //Retrieve streams from the new client socket. This is the streams where we can retrieve/send data.
+                clientMap.put(formatHash(client), client);
+                InputStream in = client.getInputStream();
+                OutputStream out = client.getOutputStream();
+                Scanner sc = new Scanner(in, "UTF-8");
 
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+                //Retrieve the initial message from client and verifying that it matches the pattern of a GET request.
+                String data = sc.useDelimiter("\\r\\n\\r\\n").next();
+                System.out.println("Request received from client: " + client.getInetAddress().toString());
+                Matcher get = Pattern.compile("^GET").matcher(data);
+
+                //Code only runs if the pattern matches get request.
+                if (get.find()) {
+
+                    //Retrieve the websocket key header data.
+                    Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+                    match.find();
+
+                    //We create the HTTP response that we will send to the client. We format it according to RFC6455.
+                    byte[] response = (
+                            "HTTP/1.1 101 Switching Protocols\r\n"
+                                    + "Connection: Upgrade\r\n"
+                                    + "Upgrade: websocket\r\n"
+                                    + "Sec-WebSocket-Accept: "
+                                    + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1")
+                                    .digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
+                                    + "\r\n\r\n").getBytes("UTF-8");
+
+                    //Write the response to the output stream.
+                    System.out.println("Responding to request from client:" + client.getInetAddress().toString());
+                    out.write(response, 0, response.length);
+                    out.flush();
+
+                    //After a successful handshake, thread is forked and listens for messages from client.
+                    Thread readThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (true) {
+                                try {
+                                    String received = decryptInput(in);
+                                    if (!received.isBlank() && !received.isEmpty()) {
+                                        System.out.println("Message received from client: " + received);
+                                        globalMessage(received);
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                    );
+                    readThread.start();
+                }
+            }
         }
     }
-
-    /*
-    public void sendResponse(OutputStream out, String mes) throws IOException{
-        byte[] responsePayload = mes.getBytes(StandardCharsets.UTF_8);
-        byte[] responseHeader = new byte[2];
-        responseHeader[0] = (byte) (0x80 | opcode);
-        responseHeader[1] = (byte) responsePayload.length;
-        out.write(responseHeader);
-        out.write(responsePayload);
-    }
-
-     */
-
-
-
-    public void sendWebSocketMessage(String message, OutputStream outputStream) throws IOException {
+    private void sendWebSocketMessage(String message, OutputStream outputStream) throws IOException {
         byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
         byte[] header = new byte[10];
         int newSize = 0;
@@ -151,8 +156,15 @@ public class WebSocket {
         outputStream.flush();
     }
 
+    private void globalMessage(String message) throws IOException {
+        for(Map.Entry<String, Socket> e : clientMap.entrySet()){
+            Socket s = e.getValue();
+            this.sendWebSocketMessage(message, s.getOutputStream());
+        }
+    }
 
-    public String decryptInput(InputStream in) throws IOException {
+
+    private String decryptInput(InputStream in) throws IOException {
         byte[] frameHeader = new byte[2];
         in.read(frameHeader);
 
@@ -200,7 +212,11 @@ public class WebSocket {
         return message;
     }
 
-        public static void main(String[] args) throws IOException {
+    private static String formatHash(Socket socket){
+        return socket.getInetAddress().toString() + ":" + socket.getPort();
+    }
+
+        public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         WebSocket ws = new WebSocket();
         ws.start();
     }
